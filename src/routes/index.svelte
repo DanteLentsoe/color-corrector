6907723ac3ff551aeac19eb7d772d109ef36d971
dantelentsoe <dllentsoe@gmail.com>
2022-05-16 18:19:39 +0200

    feat: sketch picker

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+// @ts-ignore
+
+import {HsvPicker} from 'svelte-color-picker';
+
+function colorCallback(rgba:any) {
+    console.log(rgba.detail)
+}
+</script>
+<HsvPicker on:colorChange={colorCallback} startColor={"#FBFBFB"}/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
