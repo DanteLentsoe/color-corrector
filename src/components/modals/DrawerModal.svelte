13aa833ffdac56c507a984d58be9f7ec98303b66
dantelentsoe <dllentsoe@gmail.com>
2022-05-18 18:30:41 +0200

    feat: add drawer modal

---
+++ src/components/modals/DrawerModal.svelte
@@ -0,0 +1,125 @@
+<div class="drawer-container">
+  <!-- Don't include fixed={false} if this is a page wide drawer.
+        It adds a style for absolute positioning. -->
+  <Drawer variant="modal" fixed={false} bind:open>
+    <Header>
+      <Title>Super Mail</Title>
+      <Subtitle>It's the best fake mail app drawer.</Subtitle>
+    </Header>
+    <Content>
+      <List>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Inbox')}
+          activated={active === 'Inbox'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">inbox</Graphic>
+          <Text>Inbox</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Star')}
+          activated={active === 'Star'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">star</Graphic>
+          <Text>Star</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Sent Mail')}
+          activated={active === 'Sent Mail'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">send</Graphic>
+          <Text>Sent Mail</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Drafts')}
+          activated={active === 'Drafts'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">drafts</Graphic>
+          <Text>Drafts</Text>
+        </Item>
+
+        <Separator />
+        <Subheader component={H6}>Labels</Subheader>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Family')}
+          activated={active === 'Family'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Family</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Friends')}
+          activated={active === 'Friends'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Friends</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Work')}
+          activated={active === 'Work'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Work</Text>
+        </Item>
+      </List>
+    </Content>
+  </Drawer>
+
+  <!-- Don't include fixed={false} if this is a page wide drawer.
+        It adds a style for absolute positioning. -->
+  <Scrim fixed={false} />
+      <Button on:click={() => (open = !open)}>
+        <Label>Toggle Drawer</Label>
+      </Button>
+</div>
+
+<script lang="ts">
+  import Drawer, {
+    AppContent,
+    Content,
+    Header,
+    Title,
+    Subtitle,
+    Scrim,
+  } from '@smui/drawer';
+  import Button, { Label } from '@smui/button';
+  import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+  import { H6 } from '@smui/common/elements';
+
+  let open = false;
+  let active = 'Inbox';
+
+  function setActive(value: string) {
+    active = value;
+    open = false;
+  }
+</script>
+
+<style>
+  /* These classes are only needed because the
+    drawer is in a container on the page. */
+  .drawer-container {
+    z-index: 0;
+    top:0;
+  }
+
+  * :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
+
+  .main-content {
+    overflow: auto;
+    padding: 16px;
+    height: 100%;
+    box-sizing: border-box;
+  }
+</style>
